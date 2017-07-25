3bb3cdf9cca1dd354c14048324526389e548c0ac
gaya19-meet <gaya19@meet.mit.edu>
2017-07-25 12:23:05 +0300

    Stopping here

---
+++ hello world.py
@@ -0,0 +1,33 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hello world")
+hello world
+>>> print ("hello world!")
+hello world!
+>>> ("hello Dan!")
+'hello Dan!'
+>>> #print ("hello world!")
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #print ("hello world!")
+>>> 
+====== RESTART: /home/student/gaya19_lab3/meet2017y1lab3/hello world.py ======
+hello world is how you say Hello Worldin English
+>>> 
+====== RESTART: /home/student/gaya19_lab3/meet2017y1lab3/hello world.py ======
+hello world is how you say Hello World in English
+Γειά σου Κόσμε is how you say Hello World in Greek
+Olá Mundo is how you say Hello World in Portuguese
+Ahoj světe is how you say Hello World in Czech
+안녕 세상 is how you say Hello World in Korean
+你好，世界 is how you say Hello World in Chinese(Traditional)
+नमस्ते दुनिया is how you say Hello World in Hindi
+Salamu, Dunia is how you say Hello World in Swahili
+>>> 
